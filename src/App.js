573f0e30fc69629b80c1226daa379487ec30e74c
Ming Lou <steammingliu1@gmail.com>
2021-12-09 20:37:47 -0800

    src

--- src/App.js
+++ src/App.js
@@ -1,65 +1,67 @@
-import React, {useState} from 'react'
-import Axios from 'axios'
+import React, { useState } from "react"
+import Axios from "axios"
 // import {v5 as uuidv5} from 'uuid'
 
-import Recipe from './components/Recipe/Recipe'
-import AlertMessage from './components/AlertMessage/AlertMessage'
-import chief from './img/chief.png'
-import './App.css'
+import Recipe from "./components/Recipe/Recipe"
+import AlertMessage from "./components/AlertMessage/AlertMessage"
+import chief from "./img/chief.png"
+import "./App.css"
 
+const App = () => {
+  const [foodQuery, setFoodQuery] = useState("")
+  const [foodRecipe, setFoodRecipe] = useState([])
+  const [alert, setAlert] = useState("")
 
-const App = () =>{
-    const [foodQuery, setFoodQuery] = useState("")
-    const [foodRecipe, setFoodRecipe] = useState([])
-    const [alert, setAlert] = useState("")
+  const ID = "b235abcb"
+  const KEY = "809ec57c6a89e2e94fc2eaf93b4f53cc"
+  const URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${foodQuery}&app_id=${ID}&app_key=${KEY}`
 
-    const ID = 'b235abcb'
-    const KEY = '809ec57c6a89e2e94fc2eaf93b4f53cc'	 
-    const URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${foodQuery}&app_id=${ID}&app_key=${KEY}`
+  const fetchData = async () => {
+    if (foodQuery) {
+      const fetchedData = await Axios.get(URL)
 
-    const fetchData = async () => {
-        if(foodQuery){
-        const fetchedData = await Axios.get(URL)
-
-        if(!fetchedData.data.count)
-        {
-            return setAlert(`No Food With the Name "${foodQuery}" is found!!`)
-        }
-        setFoodRecipe(fetchedData.data.hits)
-        console.log(fetchedData)
-        setFoodQuery("")
-        setAlert("")
-        }
-        else{
-            setAlert('~ Please input a food name!! ~')
-        }
-
+      if (!fetchedData.data.count) {
+        return setAlert(`No Food With the Name "${foodQuery}" is found!!`)
+      }
+      setFoodRecipe(fetchedData.data.hits)
+      console.log(fetchedData)
+      setFoodQuery("")
+      setAlert("")
+    } else {
+      setAlert("~ Please input a food name!! ~")
     }
-    
-    const onSubmit = (e) => {
-        e.preventDefault()
-        fetchData()
-    }
+  }
 
-    const onChange = (e) => {
-        setFoodQuery(e.target.value)
-    }
+  const onSubmit = (e) => {
+    e.preventDefault()
+    fetchData()
+  }
 
-    return(
-        <div className="App">
-            <h1>Dish Recipe Lookup</h1>
-            <img src={chief} alt={"chief"}></img>
-            <form onSubmit={onSubmit} className="search-form">
-                {alert && <AlertMessage alert={alert}/>}
-                <input type="text" placeholder="Anything on your mind?" autoComplete="off" onChange={onChange} value={foodQuery}/>
-                <input type="submit" value="search" />
-            </form>
-            <div className="recipes">
-                {foodRecipe && foodRecipe.map((recipe,i) => <Recipe recipe={recipe} key={i}/>)}
-            </div>
-        
-        </div>
-    )
+  const onChange = (e) => {
+    setFoodQuery(e.target.value)
+  }
+
+  return (
+    <div className="App">
+      <h1>Dish Recipe Lookup</h1>
+      <img src={chief} alt={"chief"}></img>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert && <AlertMessage alert={alert} />}
+        <input
+          type="text"
+          placeholder="Anything on your mind?"
+          autoComplete="off"
+          onChange={onChange}
+          value={foodQuery}
+        />
+        <input type="submit" value="search" />
+      </form>
+      <div className="recipes">
+        {foodRecipe &&
+          foodRecipe.map((recipe, i) => <Recipe recipe={recipe} key={i} />)}
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,19 +1,22 @@
-import React, {useState} from 'react'
-import RecipeDetails from '../RecipeDetails/RecipeDetails'
-const Recipe = ({recipe}) =>{
-    const [showIngredient, setShowIngredient] = useState(false)
+import React, { useState } from "react"
+import RecipeDetails from "../RecipeDetails/RecipeDetails"
+const Recipe = ({ recipe }) => {
+  const [showIngredient, setShowIngredient] = useState(false)
 
-
-    const {label, image, url, ingredients} = recipe.recipe
-    return (
-        <div className="recipe">
-            <h2>{label}</h2>
-            <img src={image} alt={label}></img>
-            <a href={url} target="_blank" rel="noopener noreferrer">Instruction Link</a>
-            <button onClick={()=>setShowIngredient(!showIngredient)}>Ingredients</button>
-            {showIngredient && <RecipeDetails ingredients={ingredients}/>}
-        </div>
-    )
+  const { label, image, url, ingredients } = recipe.recipe
+  return (
+    <div className="recipe">
+      <h2>{label}</h2>
+      <img src={image} alt={label}></img>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        Instruction Link
+      </a>
+      <button onClick={() => setShowIngredient(!showIngredient)}>
+        Ingredients
+      </button>
+      {showIngredient && <RecipeDetails ingredients={ingredients} />}
+    </div>
+  )
 }
 
 export default Recipe
